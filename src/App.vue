b74ecee0ef6fab73a70d2f48984bc976a6fc82f9
GitHub xbcmzdkk <GitHub 1247655689@qq.com>
2023-08-14 17:04:58 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@
 </template>
 
 <script setup lang="ts">
-var str = "我是测试str";
-const fn = () => 
-{
-  console.log('我是测试fn',str)
+var str = '我是测试str'
+const fn = () => {
+  console.log('我是测试fn', str)
 }
-fn();
+fn()
 </script>
 
 <style></style>
